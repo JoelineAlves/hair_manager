3e204640e918215b36f008fb66b11c9de8576ca0
JoelineAlves <joelinealves08@gmail.com>
2024-05-18 23:04:47 +0000

    test the register page

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
      redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -22,6 +23,11 @@ mongo = PyMongo(app)
 def get_hairs():
     hairs = mongo.db.hairs.find()
     return render_template("hairs.html", hairs=hairs)
+
+
+@app.root("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
