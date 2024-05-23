96ab75093475704bcaad6d9fac6f0c0973c7e392
Joeline Alves <joelinealves08@gmail.com>
2024-05-23 01:34:10 +0000

    seting add_hairstyle on app.py

--- app.py
+++ app.py
@@ -107,6 +107,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_hairstyle")
+def add_hairstyle():
+    return render_template("add_hairstyle.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
