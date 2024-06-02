0adfa9ca29dfe9f76d935bba4f5adc624f41b1aa
Joeline Alves <joelinealves08@gmail.com>
2024-06-02 19:29:51 +0000

    add edit category functionality

--- app.py
+++ app.py
@@ -175,7 +175,21 @@ def add_category():
         flash("New Category Added")
         return redirect(url_for("get_categories"))
 
-    return render_template("add_category.html")                                     
+    return render_template("add_category.html")
+
+
+@app.route("/edit_category/<category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    if request.method == "POST":
+        submit = {
+            "category_name": request.form.get("category_name")
+        }
+        mongo.db.categories.replace_one({"_id": ObjectId(category_id)}, submit)
+        flash("Category Successfully Updated")
+        return redirect(url_for("get_categories"))
+
+    category = mongo.db.categories.find_one({"_id": ObjectId(category_id)})
+    return render_template("edit_category.html", category=category)                                         
 
 
 
